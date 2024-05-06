b3189fbf077782e6a6a758a8cbe6c96656a7c53c
helibarros <168122468+helibarros@users.noreply.github.com>
2024-05-06 15:36:08 -0300

    FINAL

--- script.js
+++ script.js
@@ -4,9 +4,11 @@
 
 
 // Variáveis e seleções de elementos 
-const apiKey = "2a42a4beada34a433fb388b3cb4081a9"
-const cityInput = document.querySelector("#city-input")
-const searchBt = document.querySelector("#search")
+const apiKey = "2a42a4beada34a433fb388b3cb4081a9";
+const apiUnsplash = "https://source.unsplash.com/1600x900/?";
+const cityInput = document.querySelector("#city-input");
+const searchBt = document.querySelector("#search");
+
 const cityElement = document.querySelector("#city");
 const tempElement = document.querySelector("#temperature span");
 const descElement = document.querySelector("#description");
@@ -15,26 +17,69 @@ const countryElement = document.querySelector("#country");
 const umidityElement = document.querySelector("#umidity span");
 const windElement = document.querySelector("#wind span");
 
+const weatherData = document.querySelector("#weather-data");
+
+const errorMessageContainer = document.querySelector("#error-message");
+const cityErrorElement = document.querySelector("#error-message span");
+
+const loader = document.querySelector("#loader");
+
+
 //Funções
+const toggleLoader = () => {
+     loader.classList.toggle("hide");
+};
 const getWeatherData = async (city) =>{
+     toggleLoader();
+
      const apiWheatertherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
 
      const res = await fetch(apiWheatertherURL);
      const data = await res.json();
 
+     toggleLoader();
+
      return data;
 
 };
 
+//Função que recebe a cidade input, chama a função de requisição dos dados e substutui na tela os elementos html
 const showWeatherData = async(city) => {
+     //Esconde as informações a cada nova pesquisa 
+     hideInformation();
+
      const data = await getWeatherData(city);
+     
+     //verifica se a cidade existe e faz o tratamento do erro
+     if(data.cod ==="404"){
+          showErrorMessage(city);
+          return
+     }
 
      cityElement.innerText = data.name;
      tempElement.innerText = parseInt(data.main.temp);
      descElement.innerText = data.weather[0].description;
-     weatherIconElement.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
+     weatherIconElement.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+     countryElement.setAttribute('src', `https://flagsapi.com/${data.sys.country}/shiny/64.png`);
      umidityElement.innerHTML = `${data.main.humidity}%`;
      windElement.innerHTML = `${data.wind.speed} km/h`;
+
+     //Altera a imagem de fundo
+     document.body.style.backgroundImage = `url("${apiUnsplash + city}")`;
+
+     weatherData.classList.remove("hide");
+};
+
+// Funções que escondem os container
+const hideInformation = () => {
+     errorMessageContainer.classList.add("hide");
+     weatherData.classList.add("hide");
+}
+
+// tratamento de erro
+const showErrorMessage = (city) => {
+     cityErrorElement.innerText = city;
+     errorMessageContainer.classList.remove("hide");
 };
 
 //Eventos
@@ -44,5 +89,14 @@ searchBt.addEventListener("click" , (e) =>{
      const city = cityInput.value;
 
      showWeatherData(city);
+})
+
+cityInput.addEventListener("keyup", (e) => {
+     if (e.code === "Enter") {
+
+          e.preventDefault();
+          const city = e.target.value;
+          showWeatherData(city);
+     }
 })
 
